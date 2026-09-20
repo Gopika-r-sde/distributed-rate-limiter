01c5e8d8ef627c8a06f7ae3b4c18d549c9fc6b1b
gopika-r-sde <rgopikarajamani@gmail.com>
2026-09-20 18:11:48 +0530

    feat : Added ClientConfig entity

---
+++ src/main/java/com/gopika/ratelimiter/model/entity/ClientConfig.java
@@ -0,0 +1,75 @@
+package com.gopika.ratelimiter.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "client_config")
+public class ClientConfig {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String clientId;
+
+    @Column(nullable = false)
+    private int maxRequests;
+
+    @Column(nullable = false)
+    private int windowSizeInSeconds;
+
+    @Column(nullable = false)
+    private String algorithmType;
+
+    // Constructors
+    public ClientConfig() {}
+
+    public ClientConfig(String clientId, int maxRequests, int windowSizeInSeconds, String algorithmType) {
+        this.clientId = clientId;
+        this.maxRequests = maxRequests;
+        this.windowSizeInSeconds = windowSizeInSeconds;
+        this.algorithmType = algorithmType;
+    }
+
+    // Getters and Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getMaxRequests() {
+        return maxRequests;
+    }
+
+    public void setMaxRequests(int maxRequests) {
+        this.maxRequests = maxRequests;
+    }
+
+    public int getWindowSizeInSeconds() {
+        return windowSizeInSeconds;
+    }
+
+    public void setWindowSizeInSeconds(int windowSizeInSeconds) {
+        this.windowSizeInSeconds = windowSizeInSeconds;
+    }
+
+    public String getAlgorithmType() {
+        return algorithmType;
+    }
+
+    public void setAlgorithmType(String algorithmType) {
+        this.algorithmType = algorithmType;
+    }
+}
